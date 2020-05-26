76328b356e1d7bdc75248883f9267b1f3ed7a09c
Uday Yadav <dev117uday@gmail.com>
2020-05-26 17:07:43 +0530

    resume-site-v2

---
+++ resume-site-v2/public/app.js
@@ -0,0 +1,12 @@
+new Vue({
+	el: '#header-section',
+	data: {
+		name: "Uday Yadav"
+	},
+	methods: {
+		setup: function () {
+			console.log("Hello")
+		}
+	},
+
+})
---
+++ resume-site-v2/public/sw.js
@@ -0,0 +1,58 @@
+const staticAssets = [
+
+    "./",
+    "./loader.js",
+    "./manifest.json",
+    "./sw.js",
+    "./css/resume.css",
+    "./js/resume.js",
+    "./svg/"
+
+]
+
+self.addEventListener("install", async event => {
+
+    const cache = await caches.open('app-static');
+
+    cache.addAll(staticAssets);
+
+});
+
+self.addEventListener('fetch', event => {
+
+    console.log('done');
+
+    const req = event.request;
+
+    const url = new URL(req.url);
+
+    if (url.origin === location.origin) { event.respondWith(cacheFirst(req)); }
+
+    else { event.respondWith(networkFirst(req)); }
+
+})
+
+async function cacheFirst(req) {
+    const cacheResponse = await caches.match(req);
+
+    return cacheResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+
+    const cache = await caches.open('app-dyanmic');
+
+    try {
+
+        const req = await fetch(req);
+
+        cache.put(req, res.clone());
+
+        return res
+
+    } catch (error) {
+
+        return await cache.match(req);
+
+    }
+}
