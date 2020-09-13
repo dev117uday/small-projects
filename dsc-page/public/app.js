af05ba440a75e593c0b9d92460e6d5e9d2c91902
Uday Yadav <dev117uday@gmail.com>
2020-09-13 04:49:03 -0700

    added dsc page

---
+++ dsc-page/public/app.js
@@ -0,0 +1,21 @@
+new Vue({
+    el : "#box",
+    data : {
+        heading_one : "DSC ADGITM",
+        intro_body : "At DSC ADGITM, we aim to learn, teach and help developers, designers, and managers to come together under one roof to create a community that inspires thousands.",
+        first_button : "Registration Opening Soon",
+        heading_two : "Wanna a part of DSC Member Committee",
+        second_button : "Apply Now !",
+        subheading_one : "College level Community by Google",
+        subheading_two : "Build and Learn",
+        para_one : "Developer Student Club (DSC) is a community, supported by Google Developers program for college students to learn software engineering skills. ",
+        para_two : "Here you learn, share, and contribute technical skills that help you in the real world. From Web to ML, from Noob to Pro, you get to learn and build projects using cutting edge technologies.",
+        member_link : "https://www.google.com/",
+        com_link : "",
+        com_link_false : true,
+        twitter_link : "https://twitter.com/DAdgitm",
+        linkedin_link : "https://www.linkedin.com/company/developer-students-club-adgitm/?",
+        instagram_link : "",
+        github_link : "",
+    }
+})
